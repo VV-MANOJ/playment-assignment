d5622c3049909074765cf28e319499f43847708a
Ubuntu <ubuntu@ip-150-31-2-247.ap-south-1.compute.internal>
2020-10-14 19:16:11 +0000

    assignment

---
+++ hello/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def hello():
+    return jsonify({"message": "hello"})
+
+
+@app.route("/version")
+def version():
+    return jsonify({"version": "v3.0"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ world/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def world():
+    return jsonify({"message": "world"})
+
+
+@app.route("/version")
+def version():
+    return jsonify({"version": "v3.0"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=4000)
